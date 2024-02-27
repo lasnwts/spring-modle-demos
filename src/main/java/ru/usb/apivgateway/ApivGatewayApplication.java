057c5cdfe18bd0b71ae9ba6bfa5871c347765593
Alexander <alexl1967@mail.ru>
2024-02-27 20:25:31 +0300

    phase 4

---
+++ src/main/java/ru/usb/apivgateway/ApivGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.usb.apivgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApivGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/usb/apivgateway/ApivGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.usb.apivgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
